cb5b34b995aae0b5a979bcdd81c0614cd878edd8
MARIAIVANOVAZ <86656056+MARIAIVANOVAZ@users.noreply.github.com>
2021-12-03 10:03:03 +0000

    Deploying to gh-pages from @ MARIAIVANOVAZ/goit-js-hw-08@6371cc48d3af5e9e7af852758498c2fa0e34e83d 🚀

--- 02-video.92b73d6a.js
+++ 02-video.92b73d6a.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=o(require("@vimeo/player")),t=o(require("lodash.throttle"));function o(e){return e&&e.__esModule?e:{default:e}}const r=document.querySelector("iframe"),n=new Vimeo.Player(r);function l(e){localStorage.setItem("videoplayer-current-time",e.seconds),console.log(localStorage.getItem("videoplayer-current-time"))}n.on("timeupdate",(0,t.default)(l,1e3)),n.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(e){}).catch(function(e){e.name}),n.getVideoTitle().then(function(e){console.log("title:",e)});
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/src/02-video.92b73d6a.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.92b73d6a.js.map
